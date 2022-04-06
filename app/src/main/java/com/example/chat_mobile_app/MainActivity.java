e28403aa87abfbcf46b455df472b2e2488659dd4
Forty7even <foorty7even@gmail.com>
2022-04-06 23:04:44 +0200

    new commit

--- app/src/main/java/com/example/chat_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/chat_mobile_app/MainActivity.java
@@ -1,14 +1,117 @@
 package com.example.chat_mobile_app;
 
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.chat_mobile_app.databinding.ActivitySignInBinding;
+import com.example.chat_mobile_app.databinding.ActivitySignUpBinding;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+    private String encodedImage;
+    private Util ActivitySingUpBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivitySingUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private  void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBeckPressed());
+        binding.buttonSignUp.setOnClickListener(v ->{
+            if(isValidSingUpDetails()){
+                singUp();
+            };
+        });
+    }
+
+    private Object onBeckPressed() {
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT).show();
+    }
+    private void singUp(){
+
+    }
+    private String EncodedImage(Bitmap bitmap){
+        int previewWidth =  150;
+        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth,previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50,byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes,Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK){
+                    if(result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                        try{
+                            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.imageProfile.setImageBitmap(bitmap);
+                            binding.textAddImage.setVisibility(View.GONE);
+                            encodedImage = encodedImage(bitmap);
+                        }catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    )
+    private boolean isValidSingUpDetails(){
+        if(encodedImage == null){
+            showToast("Select profile image");
+            return false;
+        }else if(binding.inputName.getText().toString().trim().isEmpty()){
+            showToast("Enter name");
+            return false;
+        }else if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter email");
+            return false;
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString().matches())){
+            showToast("Enter valid image");
+            return false;
+        }else if(binding.inputConfirmPassword.getText().toString().trim().isEmpty()){
+            showToast("Confirm your password");
+            return false;
+        }else if (!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())){
+            return false;
+        }else{
+            return false;
+        }
+    }
+
+    private void loading(boolean isloading){
+        if(isloading) {
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.VISIBLE);
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/com/example/chat_mobile_app/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.example.chat_mobile_app.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IS_SINGED_IN = "isSingedIn";
+    public static final String KEY_USERS_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+}
